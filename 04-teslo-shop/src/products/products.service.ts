0f1b881483bcbd9b5f9a2145cde8bd5a254c912d
Daniel Plascencia <cdpm98@hotmail.com>
2026-04-05 13:21:05 -0600

    SECCIÓN 10 / CAP 10 - EN CURSO

---
+++ 04-teslo-shop/src/products/products.service.ts
@@ -0,0 +1,62 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PostgresError } from 'src/common/interfaces';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error instanceof QueryFailedError) {
+      const driverError: PostgresError = error.driverError as PostgresError;
+
+      if (driverError.code === '23505') {
+        throw new BadRequestException(driverError.detail);
+      }
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Ayuda!');
+  }
+}
